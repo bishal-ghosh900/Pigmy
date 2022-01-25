46dd9ebf58cf793dc18cb57cca923dedce1d1468
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2022-01-25 13:52:38 +0530

    copy button added and increases the responsiveness

--- public/handleSubmit.js
+++ public/handleSubmit.js
@@ -8,29 +8,48 @@ function handleSubmit() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ url }),
     };
+
     const main = document.querySelector("main");
-    let link = document.createElement("a");
+    const link = document.createElement("div");
+    const i = document.createElement("i");
+
     link.setAttribute("id", "result");
+
     fetch(`/api/pigmy/`, requestOptions)
       .then((res) => {
         if (res.ok) return res.json();
         else throw new Error("Not a valid url");
       })
       .then((data) => {
+        i.classList.add("fas");
+        i.classList.add("fa-copy");
+        i.setAttribute("id", "copy");
+        i.onclick = handleCopy;
         link.innerText = `${window.location.href}${data}`;
-        link.href = `${window.location.href}${data}`;
-        link.target = "_blank";
+        link.append(i);
         const lastElem = document.querySelector("#result");
         if (lastElem) main.removeChild(lastElem);
         main.append(link);
       })
       .catch((err) => {
-        link = document.createElement("div");
         link.innerText = err.message;
-        link.setAttribute("id", "result");
         const lastElem = document.querySelector("#result");
         if (lastElem) main.removeChild(lastElem);
         main.append(link);
       });
   }
+}
+
+function handleCopy() {
+  const result = document.querySelector("#result");
+  navigator.clipboard.writeText(result.textContent);
+  const i = document.querySelector("#copy");
+  i.classList.remove("fas");
+  i.classList.remove("fa-copy");
+  i.classList.add("fa");
+  i.classList.add("fa-check");
+  setTimeout(() => {
+    i.classList.add("fas");
+    i.classList.add("fa-copy");
+  }, 2000);
 }
